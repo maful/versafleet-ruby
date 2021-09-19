f950e674782f26f3dcd8aea5e9301da9c6c780e1
Maful Prayoga Arnandi <mafulprayoga@gmail.com>
2021-09-20 06:50:54 +0700

    Add API Documentation (#2)

--- lib/versafleet/client.rb
+++ lib/versafleet/client.rb
@@ -1,26 +1,53 @@
 module Versafleet
   class Client
+    # Default API endpoint
     BASE_URL = "https://api.versafleet.co/api"
 
-    attr_reader :client_id, :client_secret
+    # @return [String] Gets the VersaFleet API Key
+    attr_reader :client_id
 
+    # @return [String] Gets the VersaFleet Secret Key
+    attr_reader :client_secret
+
+    # Initialize a new VersaFlet client
+    #
+    # == Example:
+    #
+    #   client = Versafleet::Client.new(client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"])
+    #   # use Jobs resource
+    #   client.jobs.list
+    #
+    # @param client_id [String] VersaFleet API Key
+    # @param client_secret [String] VersaFleet Secret Key
     def initialize(client_id:, client_secret:)
       @client_id = client_id
       @client_secret = client_secret
     end
 
+    # Jobs Resource instance
+    #
+    # @return [JobsResource]
     def jobs
       JobsResource.new(self)
     end
 
+    # Tasks Resource instance
+    #
+    # @return [TasksResource]
     def tasks
       TasksResource.new(self)
     end
 
+    # Drivers Resource instance
+    #
+    # @return [DriversResource]
     def drivers
       DriversResource.new(self)
     end
 
+    # Initializes a new Faraday connection
+    #
+    # @return [Faraday::Connection]
     def connection
       @connection ||= Faraday.new(params: default_params) do |conn|
         conn.url_prefix = BASE_URL
--- lib/versafleet/resources/drivers.rb
+++ lib/versafleet/resources/drivers.rb
@@ -1,19 +1,64 @@
 module Versafleet
   class DriversResource < Resource
+    # List All Drivers
+    #
+    # == Examples:
+    #
+    #   client.drivers.list
+    #   # set per page to 20
+    #   client.drivers.list(per_page: 20)
+    #   # move to page 2
+    #   client.drivers.list(page: 2, per_page: 20)
+    #
+    # {https://versafleet.docs.apiary.io/#reference/0/drivers-api/list-all-drivers VersaFleet API}
+    #
+    # @param params [Hash] the filter query
+    # @return [Collection]
     def list(**params)
       response = get_request("drivers", params: params)
       Collection.from_response(response, key: "drivers", type: Driver)
     end
 
+    # Get Driver details
+    #
+    # == Examples:
+    #
+    #   client.drivers.retrieve(driver_id: 123)
+    #
+    # {https://versafleet.docs.apiary.io/#reference/0/drivers-api/show-a-driver VersaFleet API}
+    #
+    # @param driver_id [Integer] Driver ID
+    # @return [Driver]
     def retrieve(driver_id:)
       Driver.new get_request("drivers/#{driver_id}").body.dig("driver")
     end
 
+    # Create a Driver
+    #
+    # == Examples:
+    #
+    #   client.drivers.create(driver: {name: "Yolo", dob: "1990-09-09"})
+    #
+    # {https://versafleet.docs.apiary.io/#reference/0/drivers-api/create-a-driver VersaFleet API}
+    #
+    # @param driver [Hash] Driver request payload
+    # @return [Driver]
     def create(driver:)
       payload = {driver: driver}
       Driver.new post_request("drivers", body: payload).body.dig("driver")
     end
 
+    # Update Driver
+    #
+    # == Examples:
+    #
+    #   client.drivers.update(driver_id: 123, driver: {name: "John"})
+    #
+    # {https://versafleet.docs.apiary.io/#reference/0/drivers-api/update-a-driver VersaFleet API}
+    #
+    # @param driver_id [Integer] Driver ID
+    # @param driver [Hash] Driver request payload
+    # @return [Driver]
     def update(driver_id:, driver:)
       payload = {driver: driver}
       Driver.new put_request("drivers/#{driver_id}", body: payload).body.dig("driver")
